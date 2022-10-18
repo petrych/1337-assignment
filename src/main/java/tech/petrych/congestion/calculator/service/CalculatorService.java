d8b7ac9be247d0b3a4fa34b37d4dfe47dc6150e5
Anastasiia Petrych <a.petrych@gmail.com>
2022-10-18 11:28:36 +0200

    Refactor: When calculating max fee, use a list data structure

--- src/main/java/tech/petrych/congestion/calculator/service/CalculatorService.java
+++ src/main/java/tech/petrych/congestion/calculator/service/CalculatorService.java
@@ -5,6 +5,9 @@ import org.springframework.stereotype.Service;
 import tech.petrych.congestion.calculator.model.IVehicle;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 import java.util.concurrent.TimeUnit;
 
 import static tech.petrych.congestion.calculator.service.DateUtil.toMillis;
@@ -24,25 +27,25 @@ public class CalculatorService implements ICalculatorService {
 		
 		LocalDateTime intervalStart = dates[0];
 		int totalFee = 0;
-		int maxFee = 0;
+		List<Integer> intervalFees = new ArrayList<>();
 		
 		for (int i = 0; i < dates.length; i++) {
 			LocalDateTime date = dates[i];
 			int nextFee = getTollFee(date, vehicle);
-			if (maxFee == 0)
-				maxFee = getTollFee(intervalStart, vehicle);
+			if (intervalFees.isEmpty())
+				intervalFees.add(getTollFee(intervalStart, vehicle));
 			
 			long diffInMillies = toMillis(date) - toMillis(intervalStart);
 			long seconds = TimeUnit.MILLISECONDS.toSeconds(diffInMillies);
 			
 			if (seconds < 3600) {
-				if (totalFee > 0) totalFee -= maxFee;
-				if (nextFee >= maxFee) maxFee = nextFee;
-				totalFee += maxFee;
+				if (totalFee > 0) totalFee -= Collections.max(intervalFees);
+				intervalFees.add(nextFee);
+				totalFee += Collections.max(intervalFees);
 			} else {
 				intervalStart = dates[i];
 				totalFee += nextFee;
-				maxFee = 0;
+				intervalFees.clear();
 			}
 			
 			if (totalFee > 60) return 60;
