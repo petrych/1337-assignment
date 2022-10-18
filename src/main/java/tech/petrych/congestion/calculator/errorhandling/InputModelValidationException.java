14188841bf6dff16a188e14e2c3da7794602acb1
Anastasiia Petrych <a.petrych@gmail.com>
2022-10-18 08:18:58 +0200

    Refactor InputModelValidationException

--- src/main/java/tech/petrych/congestion/calculator/errorhandling/InputModelValidationException.java
+++ src/main/java/tech/petrych/congestion/calculator/errorhandling/InputModelValidationException.java
@@ -2,6 +2,11 @@ package tech.petrych.congestion.calculator.errorhandling;
 
 public class InputModelValidationException extends RuntimeException {
 	
+	public static final String NO_INFORMATION_ABOUT_THE_DATES = "No information about the dates.";
+	
+	public static final String NO_INFORMATION_ABOUT_THE_VEHICLE = "No information about the vehicle.";
+	
+	
 	public InputModelValidationException(String message) {
 		
 		super(message);
--- src/main/java/tech/petrych/congestion/calculator/web/CongestionTaxCalculatorController.java
+++ src/main/java/tech/petrych/congestion/calculator/web/CongestionTaxCalculatorController.java
@@ -1,28 +1,20 @@
 package tech.petrych.congestion.calculator.web;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import tech.petrych.congestion.calculator.errorhandling.InputModelValidationException;
-import tech.petrych.congestion.calculator.model.IVehicle;
 import tech.petrych.congestion.calculator.service.ICalculatorService;
 import tech.petrych.congestion.calculator.service.IVehicleService;
 
 import java.time.LocalDateTime;
-import java.util.Objects;
 
 import static tech.petrych.congestion.calculator.service.DateUtil.sortDates;
 
 @RestController
 public class CongestionTaxCalculatorController {
-	
-	private static final Logger LOG = LoggerFactory.getLogger(CongestionTaxCalculatorController.class);
 	
 	@Autowired
 	ICalculatorService service;
@@ -44,21 +36,13 @@ public class CongestionTaxCalculatorController {
 		LocalDateTime[] dates = inputModel.getLocalDates();
 		
 		if (dates == null || dates.length == 0) {
-			String errorMessage = "No information about the dates.";
-			InputModelValidationException ex = new InputModelValidationException(errorMessage);
-			LOG.error(errorMessage, ex);
-			
-			throw ex;
+			throw new InputModelValidationException(InputModelValidationException.NO_INFORMATION_ABOUT_THE_DATES);
 		}
 		
 		String vehicleType = inputModel.getVehicleType();
 		
 		if (vehicleType == null || vehicleType.isEmpty()) {
-			String errorMessage = "No information about the vehicle (null).";
-			InputModelValidationException ex = new InputModelValidationException(errorMessage);
-			LOG.error(errorMessage, ex);
-		
-			throw ex;
+			throw new InputModelValidationException(InputModelValidationException.NO_INFORMATION_ABOUT_THE_VEHICLE);
 		}
 	}
 	
